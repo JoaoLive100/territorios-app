d9e0ed303d8accd7067adc1d14c2e8e24187ff57
João Rodrigues Parra <j-r-p@outlook.com>
2023-12-20 14:24:29 -0300

    pwa

---
+++ app/src/main/java/domain/app/territorios/DelegationService.java
@@ -0,0 +1,14 @@
+package domain.app.territorios;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
